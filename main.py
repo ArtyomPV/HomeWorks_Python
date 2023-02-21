b3378a4ce555c98b28b2827f55a6db3506b55340
Artyom <itcoding76@gmail.com>
2023-02-21 18:31:10 +0200

    Third task is done

--- main.py
+++ main.py
@@ -23,15 +23,44 @@
 # 24 -> 4  16  4
 # 60 -> 10  40  10
 
-number_Crane_bird = int(input('Введите количество журавликов: \n'))
-petya = None
-sereja = None
-katya = None
+# number_Crane_bird = int(input('Введите количество журавликов: \n'))
+# petya = None
+# sereja = None
+# katya = None
 
-sereja = number_Crane_bird//6
+# sereja = number_Crane_bird//6
 
-petya = sereja
-katya = number_Crane_bird-(petya + sereja)
+# petya = sereja
+# katya = number_Crane_bird-(petya + sereja)
 
-print(petya, katya, sereja)
+# print(petya, katya, sereja)
+
+# ==================================================================
+# Задача 6: Вы пользуетесь общественным транспортом? Вероятно,
+# вы расплачивались за проезд и получали билет с номером.
+# Счастливым билетом называют такой билет с шестизначным номером,
+# где сумма первых трех цифр равна сумме последних трех.
+# Т.е. билет с номером 385916 – счастливый, т.к. 3+8+5=9+1+6.
+# Вам требуется написать программу, которая проверяет счастливость билета.
+#
+# *Пример:*
+#
+# 385916 -> yes
+# 123456 -> no
+
+bilet_Number = int(input("Введите номер билета: \n"))
+num1 = bilet_Number//1000
+num2 = bilet_Number%1000
+sum1, sum2 = 0, 0
+
+while num1 != 0 and num2 != 0:
+    sum1 += num1 % 10
+    num1 //= 10
+    sum2 += num2 % 10
+    num2 //= 10
+
+if sum != sum:
+    print("no")
+else:
+    print("yes")
 
